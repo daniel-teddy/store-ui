99a5db651c14d3ea171cae0dbdd801845486b5d1
teddy-webdev <89258830+teddy-webdev@users.noreply.github.com>
2023-09-21 16:19:31 +0300

    made form

--- public/contact-submit.js
+++ public/contact-submit.js
@@ -3,6 +3,7 @@ const closeCT = document.querySelector('#closeCT')
 const main_section = document.querySelector('#main_section')
 const condition_section = document.querySelector('#condition_section')
 
+const submit_form = document.querySelector('#submit_button')
 
 closeCT.addEventListener('click',  () => {
     main_section.classList.remove("none");
@@ -17,22 +18,31 @@ openCT.addEventListener('click',  () => {
     });
 })
 
-function validateSection() {
-    const inputs = main_section.querySelectorAll('input[required]');
+submit_form.addEventListener('click', () => {
+    const inputs = main_section.querySelectorAll('input');
 
     let isValid = true;
 
-    inputs.forEach(input => {
-        if (!input.value.trim()) {
-            isValid = false;
-        }
-    });
+    // inputs.forEach(input => {
+    //     if (!input.value.trim()) {
+    //         isValid = false;
+    //     }
+    // });
 
     if (isValid) {
         // All fields are filled correctly
-        alert('Section is valid! Processing...');
-        // Add your logic for processing the section data here
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbzBGBNUEiRXEZ_yNeyXFc5liDfZcARlbM9KKozu6g8kHzXTYaM1-2xNUCBDC3i2zSecMQ/exec'
+      const form = document.forms['myform']
+
+      submit_form.addEventListener('click', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+          .then(response => alert("Thank you! your form is submitted successfully."))
+          .then(() => { window.location.reload(); })
+          .catch(error => console.error('Error!', error.message))
+      })
     } else {
-        alert('Please fill in all required fields.');
+        alert('Please fill in all required fields. make sure you upload all ID images');
     }
-}
+})
+
